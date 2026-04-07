307bad6e4ebdc920c6f078bfb32ff815cd6a05c7
Biraj Chhetri <birajchhetri3824@gmail.com>
2026-04-07 16:48:33 +0545

    Initial commit

---
+++ screens/login_screen.py
@@ -0,0 +1,9 @@
+class LoginScreen():
+    _USERNAME_FIELD_ID = 'username'
+    _PASSWORD_FIELD_ID = 'password'
+    _LOGIN_BUTTON_ID = '-esm-login__login__continueButton___VnN+0 cds--btn cds--btn--primary' # class
+
+    def __init__(self) -> None:
+        self.username_field = Button(
+            
+        )
